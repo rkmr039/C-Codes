51cd4865df13ccbd45827a30e4f0f678b82e8006
Rishab Kumar <rkmr039@gmail.com>
2018-08-17 10:53:44 +0530

    reverse string passed from command line argument

--- CLA_STR.c
+++ CLA_STR.c
@@ -1,24 +1,18 @@
 #include<stdio.h>
-#include<stdlib.h>
-#include<conio.h>
-#include<string.h>
 
 int main(int argc, char *argv[])
 {
 	char *ptr = argv[1];
 	int len=0;
-	int i;
-
 	while(*ptr != '\0')
 	{
-		len++;
-		ptr++;
+		len++; // get string length
+		ptr++;// increase pointer upto last character 
 	}
-	ptr--;
-	while(len--)
+	ptr--; // avoid '\0' at end of string 
+	while(len--) // while length > 0 
 	{
 		printf("%c",*ptr);ptr--;
 	}
-
 	return 0;
 }
